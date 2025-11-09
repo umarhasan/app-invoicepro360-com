e33353516f16a60e79e9d2eb621172a648ced970
umar <umar.farooq@ingenicotribe.com>
2025-11-10 02:41:35 +0500

    16

---
+++ src/global-components/Alert/Danger.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        class="alert alert-outline-danger alert-dismissible mb-2 yr-danger bg-white text-normal tracking-wide show"
+        :class="{'flex items-center': !slots.header}"    
+        role="alert">
+
+        <slot name="header" v-if="slots.header" class="flex items-center"></slot>
+
+        <Lucide icon="AlertOctagon" class="w-6 h-6 mr-2" v-if="!slots.header"/> 
+        <slot />
+        <button type="button" class="btn-close" data-bs-dismiss="alert"
+            aria-label="Close"
+            v-if="!slots.header">
+            <Lucide icon="X" class="w-4 h-4" />
+        </button>
+    </div>
+</template>
+
+<script>
+import { defineComponent, useSlots } from "vue"
+
+export default defineComponent({
+    setup (){
+        const slots = useSlots()
+
+        return {
+            slots
+        }
+    }
+})
+</script>
---
+++ src/global-components/Alert/Success.vue
@@ -0,0 +1,23 @@
+<template>
+    <div
+        class="alert alert-outline-success alert-dismissible flex items-center mb-2 yr-success bg-white text-normal tracking-wide show"
+        :class="cssClass"
+        role="alert">
+         <slot />
+
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    props : ['cssClass'],
+    setup(props){
+
+        return {
+            cssClass : props.cssClass            
+        }
+    }
+})
+</script>
