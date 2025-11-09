4f4973d079d705b7ab703b82f529ee1acf436c2c
umar <umar.farooq@ingenicotribe.com>
2025-11-10 02:20:31 +0500

    first

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: [
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+  ],
+  rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/no-multiple-template-root" : "on"
+  },
+  globals: {
+    cash: true,
+    $nuxt: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
